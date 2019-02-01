af75481b62ae1c4aefc04f4ddaf0164d624e1899
Thierry <thierry.soares@inmetrics.com.br>
2019-02-01 10:46:08 -0200

    "xpath do txt pesquisa diiminuido"

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -11,8 +11,8 @@ class Home
      end
  
      def txtpesquisa()
-          binding.pry
-        $appium_driver.find_element(:xpath, "//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.EditText").click
+          
+        $appium_driver.find_element(:xpath, "//android.widget.LinearLayout/android.widget.EditText").click
      end
    
      def permissaoclk()
@@ -22,7 +22,7 @@ class Home
 
      def inserepesquisa(pesquisa)
           
-          $appium_driver.find_element(:xpath, "//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.EditText").send_keys(pesquisa)     
+          $appium_driver.find_element(:xpath, "//android.widget.LinearLayout/android.widget.EditText").send_keys(pesquisa)     
      end
 
      def resultado(result)
